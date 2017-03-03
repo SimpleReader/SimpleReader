42750804dd641bd386d2f920271b63b3e1afc6b7
gexiangjie <gxj341224>
2017-03-03 21:20:49 +0800

    bottomNavigation

--- simplereader/src/main/java/com/simplereader/graduation/ui/MainActivity.java
+++ simplereader/src/main/java/com/simplereader/graduation/ui/MainActivity.java
@@ -1,29 +1,92 @@
 package com.simplereader.graduation.ui;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.Menu;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.simplereader.graduation.base.BaseActivity;
-import com.simplereader.graduation.bean.Gank;
-import com.simplereader.graduation.mvp.contract.MainContract;
-import com.simplereader.graduation.mvp.presenter.MainPresenter;
 import com.simplereader.simplereader.R;
 
-import java.util.List;
-import java.util.Random;
+public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends BaseActivity<MainPresenter>
-        implements MainContract.View {
+    BottomNavigationView bottomNavigationView;
+    ViewPager viewPager;
+    MenuItem prevMenuItem;
 
-    private static final String TAG = MainActivity.class.getSimpleName();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        switch (item.getItemId()) {
+                            case R.id.item_call:
+                                viewPager.setCurrentItem(0);
+                                break;
+                            case R.id.item_mail:
+                                viewPager.setCurrentItem(1);
+                                break;
+                            case R.id.item_person:
+                                viewPager.setCurrentItem(2);
+                                break;
+                        }
+                        return false;
+                    }
+                });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                if (prevMenuItem != null) {
+                    prevMenuItem.setChecked(false);
+                } else {
+                    bottomNavigationView.getMenu().getItem(0).setChecked(false);
+                }
+                bottomNavigationView.getMenu().getItem(position).setChecked(true);
+                prevMenuItem = bottomNavigationView.getMenu().getItem(position);
+
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+        // 如果想禁止滑动，可以把下面的代码取消注释
+//        viewPager.setOnTouchListener(new View.OnTouchListener() {
+//            @Override
+//            public boolean onTouch(View v, MotionEvent event) {
+//                return true;
+//            }
+//        });
+
+        setupViewPager(viewPager);
+    }
+
+    private void setupViewPager(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+
+        adapter.addFragment(DemoFragment.newInstance("拨号"));
+        adapter.addFragment(DemoFragment.newInstance("信息"));
+        adapter.addFragment(DemoFragment.newInstance("我的"));
+        viewPager.setAdapter(adapter);
+    }
+
+   /* private static final String TAG = MainActivity.class.getSimpleName();
 
     private ProgressDialog mDialog;
     private Toolbar mToolbar;
@@ -99,5 +162,5 @@ public class MainActivity extends BaseActivity<MainPresenter>
             return true;
         }
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
